2d256ff0e6efe97cc74239b74a44073d5340d0ce
Elise Farley <efarley@bsd220.org>
2020-03-04 10:49:59 -0600

    Added segue to location details view controller

---
+++ 'Za/LocationDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationDetailsViewController.swift
+//  'Za
+//
+//  Created by Elise Farley on 3/4/20.
+//  Copyright © 2020 Elise Farley. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
