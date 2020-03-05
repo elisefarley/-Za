9945878b6606b8b3e8590cd8c79c7a868dad5623
Elise Farley <efarley@bsd220.org>
2020-03-05 10:28:11 -0600

    Added directions and website buttons

--- 'Za/LocationDetailsViewController.swift
+++ 'Za/LocationDetailsViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Elise Farley. All rights reserved.
 //
 
+import SafariServices
 import MapKit
 import UIKit
 
@@ -31,5 +32,16 @@ class LocationDetailsViewController: UIViewController {
         address += selectedMapItem.placemark.postalCode!
         addressLabel.text = address
         phoneLabel.text = selectedMapItem.phoneNumber
+    }
+    
+    @IBAction func onDirectionsButtonTapped(_ sender: Any) {
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
+        MKMapItem.openMaps(with: [selectedMapItem], launchOptions: launchOptions)
+    }
+    
+    @IBAction func onWebsiteButtonTapped(_ sender: Any) {
+        if let url = selectedMapItem.url {
+           present(SFSafariViewController(url: url), animated: true)
+        }
     }
 }
